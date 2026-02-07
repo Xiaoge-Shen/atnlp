9419fc552cb622af6a0e11884034e38b2a52bba7
Shay Cohen <scohen@Shays-MacBook-Pro.local>
2026-02-07 23:18:35 +0000

    commit

--- 2026/coursework/partI/shapley_value_evaluation.py
+++ 2026/coursework/partI/shapley_value_evaluation.py
@@ -2,6 +2,13 @@ import pandas as pd
 import itertools
 import numpy as np
 
+
+### IMPORTANT NOTE: In the comments that include "Input" and "Output" you should consider mostly the *structure*
+### of the input and the output, rather than the specific inputs match to the output as you understand.
+### In some cases, the output is divorced from the input above, to not make filling in the function too easy.
+### Focus on the API through these comments, in the sense that, ensure that the input and the output of the functions is correct
+### (and of course, semantically correct according to your understanding).
+
 steps = [1, 2, 3, 4]
 
 def get_included_steps(row, steps):
